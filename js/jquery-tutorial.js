418b935aa0b62d3c9034a93c264eb56fe5dc6ce3
Jialin Wu <alexsixdegrees@outlook.com>
2018-04-07 15:27:08 +0800

    final update

---
+++ js/jquery-tutorial.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+// 	// // jquery selector:
+// 	// $("button").click(function(){
+// 	// 	// $("p.intro").hide(); // select all p that class="intro"
+// 	// 	// $("p#intro").hide(); // select all p that id="intro"
+
+// 	// // select all p elements
+// 	// $("p").hide();
+// 	// });
+
+// 	function chatAdd(){
+// 		var txt1="<p>Attention!!!!!</p>";
+// 		$("body").append(txt1);  
+// 	}
+// 	$("button").click(function chatAdd())
+// });
+
